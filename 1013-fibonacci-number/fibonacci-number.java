4eace6fb6fd90536547cdebcc071002a17c4c9bb
Anushka-1406 <anushkabs08@gmail.com>
2025-08-19 20:11:42 +0530

    Update fibonacci-number.java

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -8,3 +8,12 @@ class Solution {
         
     }
 }
+/**
+ * Time Complexity (TC): O(2^n)
+ * - The function makes two recursive calls for each value of n, resulting in an exponential growth in the number of function calls.
+ * - This results in a total time complexity of O(2^n) because the same Fibonacci numbers are recalculated multiple times.
+
+ * Space Complexity (SC): O(n)
+ * - The space complexity is due to the call stack depth of the recursion.
+ * - The maximum depth of the recursion stack is n, so the space complexity is O(n).
+ */
