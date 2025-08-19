1480688b9314a913467cc8a64d64578359b247d9
Anushka-1406 <anushkabs08@gmail.com>
2025-08-19 20:21:22 +0530

    Time: 10 ms (10.00%) | Memory: 40.3 MB (63.83%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,3 +1,4 @@
+//Approach:using multiple recursion calls
 class Solution {
     public int fib(int n) {
         if(n==0)
@@ -8,12 +9,3 @@ class Solution {
         
     }
 }
-/**
- * Time Complexity (TC): O(2^n)
- * - The function makes two recursive calls for each value of n, resulting in an exponential growth in the number of function calls.
- * - This results in a total time complexity of O(2^n) because the same Fibonacci numbers are recalculated multiple times.
-
- * Space Complexity (SC): O(n)
- * - The space complexity is due to the call stack depth of the recursion.
- * - The maximum depth of the recursion stack is n, so the space complexity is O(n).
- */
